83b73dc30aef1f80fa44dde92541987d247f403b
JonathonMSmith <36175570+JonathonMSmith@users.noreply.github.com>
2020-04-16 14:55:38 -0400

    added CHAIN boilerplate

--- chain.py
+++ chain.py
@@ -1,5 +1,12 @@
 """
 Loads data from the Chain network
+
+CHAIN has 25 GNSS Ionospheric Scintillation and TEC Monitors (GISTM) located
+throughout the Canadian Arctic. Each GISTM operates as a dual-band
+Global Navigation Satellite System (GNSS) receiver as well as a GNSS Ionospheric
+Scintillation and TECM Monitor (GISTM). GNSS data is collected at 1Hz, while
+GISTM data is collected at 50Hz.
+
 """
 import georinex
 
